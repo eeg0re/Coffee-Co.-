8b1ea7b1bab497dcbeb03e87b1a44cc3029aac53
Igor Bessa <ibessa@ucsc.edu>
2024-10-16 14:40:12 -0700

    got basic drawing working, Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,41 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+const context = canvas.getContext("2d");// get canvas context so we can draw
+const cursor = {
+    active: false, 
+    x: 0,
+    y: 0,
+};
+
+// -------------- add event listeners to the canvas --------------------
+canvas.addEventListener("mousedown", (event)=>{         // if mousedown happens, activate the cursor
+    cursor.active = true;
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+});
+
+canvas.addEventListener("mousemove", (event)=>{
+    if(cursor.active && context){
+        context.beginPath();
+        context.moveTo(cursor.x, cursor.y);
+        context.lineTo(event.offsetX, event.offsetY);
+        context.stroke();
+        cursor.x = event.offsetX;
+        cursor.y = event.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", ()=>{ cursor.active = false; });
+// --------------------------------------------------------------------
+
+// add a button for clearing the page
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+app.append(clearButton);
+clearButton.addEventListener("click", ()=>{
+    if(context){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
