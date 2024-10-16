9a6f89485fe88d98f076968466e230459e1a05d6
Igor Bessa <ibessa@ucsc.edu>
2024-10-16 14:22:03 -0700

    added a canvas, Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,15 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+
+// add an app header
+const header = document.createElement("h1");
+header.innerHTML = "Digital Doodler";
+app.append(header);
+
+// add a canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
