2a7a1f6ad5138553f52f5a366bbb1ef1bd31254e
Igor Bessa <ibessa@ucsc.edu>
2024-10-19 18:53:42 -0700

    refactored, working on selected tool display, Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -38,17 +38,18 @@ interface point{
 // make an interface with lines display/drag methods
 interface drawableLine{
     points: point[],
-    display(context: CanvasRenderingContext2D): void;
-    drag(x:number, y:number): void;
-}
+    display(context: CanvasRenderingContext2D): void,
+    drag(x:number, y:number): void,
+    lineWidth: number,
+};
 
 // factory function for making drawableLines
-function createDrawableLine(initX: number, initY: number): drawableLine{
+function createDrawableLine(initX: number, initY: number, lineWidth: number): drawableLine{
     const points: point[] = [{x: initX, y: initY}];
 
     return {
         points, 
-
+        lineWidth, 
         drag(x:number, y:number){
             points.push({x, y});
         },
@@ -61,6 +62,7 @@ function createDrawableLine(initX: number, initY: number): drawableLine{
                 for (const point of this.points){
                     context.lineTo(point.x, point.y);
                 }
+                context.lineWidth = this.lineWidth; 
                 context.stroke();
             }
             
@@ -68,13 +70,16 @@ function createDrawableLine(initX: number, initY: number): drawableLine{
     }
 }
 
+let currentLineWidth = 1; 
+
 // -------------- add event listeners to the canvas --------------------
-canvas.addEventListener("mousedown", (event)=>{         // if mousedown happens, activate the cursor
+canvas.addEventListener("mousedown", (event)=>{         
+    // if mousedown happens, activate the cursor
     cursor.active = true;
     cursor.x = event.offsetX;
     cursor.y = event.offsetY;
-    
-    currentLine = createDrawableLine(cursor.x, cursor.y);
+
+    currentLine = createDrawableLine(cursor.x, cursor.y, currentLineWidth);
 
     lines.push(currentLine);
     redoLines.splice(0, redoLines.length);
@@ -89,7 +94,13 @@ canvas.addEventListener("mousemove", (event)=>{
         cursor.y = event.offsetY;
         currentLine.points.push({x: cursor.x, y: cursor.y});
 
-        draw();
+        ctx.beginPath();
+        if(currentLine.points.length > 1){
+            const len = currentLine.points.length;
+            ctx.moveTo(currentLine.points[len-2].x, currentLine.points[len - 2].y);
+            ctx.lineTo(cursor.x, cursor.y);
+            ctx.stroke();
+        }
     }
 });
 
@@ -99,7 +110,7 @@ canvas.addEventListener("mouseup", ()=>{
 });
 
 canvas.addEventListener("drawing changed", ()=>{
-    redraw();
+    DisplayLines();
 })
 
 // --------------------------------------------------------------------
@@ -114,8 +125,8 @@ function draw(){
     canvas.dispatchEvent(drawingChanged);
 }
 
-// our redraw function
-function redraw(){
+// function that calls the display method
+function DisplayLines(){
     if(ctx){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (const line of lines){
@@ -124,46 +135,88 @@ function redraw(){
     }
 }
 
-// add a break in between our canvas and buttons
-document.body.append(document.createElement("br"));
+interface button{
+    label: string,
+    color: string,
+    type: string, 
+    callback: Function,
+};
 
-// add a button for clearing the page
-const clearButton = document.createElement("button");
-clearButton.innerHTML = "Clear";
-app.append(clearButton);
-clearButton.addEventListener("click", ()=>{
-    if(ctx){
-        lines.splice(0, lines.length);
-        draw();
-    }
-});
-
-// add a button for undoing 
-const undoButton = document.createElement("button");
-undoButton.innerHTML = "Undo";
-app.append(undoButton);
-
-undoButton.addEventListener("click", ()=> {
-    if(lines && lines.length > 0){  // check if there's actually anything to undo
-        const newestLine: drawableLine | undefined = lines.pop();  
-        if(newestLine){                         // make sure we have a line to undo
-            redoLines.push(newestLine);
-            draw();
+const buttons:button[] = [
+    {
+        label: "Thin marker",
+        color: "#29415c",
+        type: "tool",
+        callback: () => {
+            currentLineWidth = 1;
+        },
+    },
+    {
+        label: "Thick marker",
+        color: '',
+        type: "tool",
+        callback: () => {
+            currentLineWidth = 3;
+        }
+    },
+    {
+        label: "Clear",
+        color: '',
+        type: "functional",
+        callback: () => {
+            if(ctx){
+                lines.splice(0, lines.length);
+                draw();
+            }
+        }
+    },
+    {
+        label: "Undo",
+        color: '',
+        type: "functional",
+        callback: () => {
+            if(lines && lines.length > 0){  // check if there's actually anything to undo
+                const newestLine: drawableLine | undefined = lines.pop();  
+                if(newestLine){                         // make sure we have a line to undo
+                    redoLines.push(newestLine);
+                    draw();
+                }
+            }
+        }
+    },
+    {
+        label: "Redo",
+        color: '',
+        type: "functional",
+        callback: () => {
+            if(redoLines && redoLines.length > 0){
+                const newRedoLine: drawableLine | undefined = redoLines.pop();
+                if(newRedoLine){
+                    lines.push(newRedoLine);
+                    draw();
+                }
+            }
         }
     }
-});
+];
 
-// add a button for redoing 
-const redoButton = document.createElement("button");
-redoButton.innerHTML = "Redo";
-app.append(redoButton);
-
-redoButton.addEventListener("click", ()=> {
-    if(redoLines && redoLines.length > 0){
-        const newRedoLine: drawableLine | undefined = redoLines.pop();
-        if(newRedoLine){
-            lines.push(newRedoLine);
-            draw();
+function MakeButtonsFromList(buttons: button[]){
+    let lastButtonType: string = '';
+    for (let i = 0; i < buttons.length; i++){
+        if(buttons[i].type != lastButtonType){
+            app.append(document.createElement("br"));
         }
+        lastButtonType = buttons[i].type;
+        MakeButton(buttons[i]);
     }
-});
+}
+
+function MakeButton(buttn: button){
+    const thisButton = document.createElement("button");
+    thisButton.innerHTML = buttn.label;
+    app.append(thisButton);
+    thisButton.style.backgroundColor = buttn.color;
+    thisButton.addEventListener('click', () => { buttn.callback() } );
+}
+
+MakeButtonsFromList(buttons);
