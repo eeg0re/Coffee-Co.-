49f5d611183b7def3214276198347d186b962219
Igor Bessa <ibessa@ucsc.edu>
2024-12-18 15:06:37 -0800

    added basic inventory that updates with clicker count

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import "./style.css";
 
+let drinkCounter: number = 0;
+
+function UpdateInventory() {
+  inventory.innerHTML = `Drinks Brewed: ${drinkCounter}`;
+}
+
+function IncreaseClickCounter() {
+  drinkCounter++;
+  UpdateInventory();
+}
+
 const GAME_NAME = "Coffee Co.";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
@@ -9,10 +20,14 @@ const header = document.createElement("h3");
 header.innerHTML = GAME_NAME;
 app.append(header);
 
-const mainClicker = document.createElement("button");
-mainClicker.innerHTML = "Click me!";
-app.append(mainClicker);
+// portion of the UI that displays the inventory
+const inventory = document.querySelector<HTMLDivElement>("#inventory")!;
+inventory.innerHTML = "Drinks Brewed: 0";
 
+// our main button for brewing coffee
+const mainClicker = document.createElement("button");
+mainClicker.innerHTML = "☕️";
+app.appendChild(mainClicker);
 // Add some sauce to it
 mainClicker.style.width = "200px";
 mainClicker.style.height = "200px";
@@ -20,10 +35,3 @@ mainClicker.style.fontSize = "60px";
 mainClicker.style.borderRadius = "50%";
 mainClicker.style.boxShadow = "5px 5px 15px rgba(0, 0, 0, 0.3)";
 mainClicker.addEventListener("click", IncreaseClickCounter);
-
-let drinkCounter: number = 0;
-
-function IncreaseClickCounter() {
-  drinkCounter++;
-  mainClicker.innerHTML = `☕️ ${drinkCounter}`;
-}
